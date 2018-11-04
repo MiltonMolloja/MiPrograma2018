16e616cb0dabeb8babdf7a38993dcac2b7c28174
Milton Molloja <38334253+MiltonMolloja@users.noreply.github.com>
2018-11-04 19:22:15 -0300

    Initial commit

---
+++ src/pka/Clase.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pka;
+
+/**
+ * @author milto
+ *
+ */
+public class Clase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+
+}
